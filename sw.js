ab8f28d18f393c88e58e9d3934bb281d8cb59d1d
rlatmdwn11 <tmdwn0515@naver.com>
2026-08-01 20:18:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME='todaycock2-final-v20';
+const CACHE_NAME='todaycock2-final-mobile-v20';
 const ASSETS=[
   './','./index.html','./viewer.html','./firebase-config.js',
-  './css/style.css','./js/app.js','./js/schedule.js','./js/firebase.js','./manifest.json'
+  './style.css','./app.js','./schedule.js','./firebase.js','./manifest.json'
 ];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
