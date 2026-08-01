954e914a8507366e2be47f41fd00046f26fdd4b5
rlatmdwn11 <tmdwn0515@naver.com>
2026-08-01 16:27:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='todaycock-pwa-v24';
+const CACHE='todaycock-pwa-v25';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./todaycock-qr.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
