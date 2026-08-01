c0a2211930085911086a61be51ecec83c0517ec3
rlatmdwn11 <tmdwn0515@naver.com>
2026-08-01 17:51:10 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.TODAYCOCK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDGUuN7Lo1Jw5wwl29NO7vg379RYbTvQy8",
+  authDomain: "todaycock-5c0fc.firebaseapp.com",
+  databaseURL: "https://todaycock-5c0fc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todaycock-5c0fc",
+  storageBucket: "todaycock-5c0fc.firebasestorage.app",
+  messagingSenderId: "272533637212",
+  appId: "1:272533637212:web:a4d8df5b15e806c035fe99"
+};
+
+window.TODAYCOCK_ADMIN_UID = "gl3RQYerLaPZJSyX6ZGeSCudWIF2";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='todaycock-pwa-v40';
+const CACHE='todaycock-pwa-v42';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./todaycock-qr.png','./viewer.html','./firebase-config.js'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
